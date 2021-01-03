bebbd456e1f8689307b2d03d4485bc6d1275a615
bassem cherni <bassemcherni1256@gmail.com>
2021-01-03 17:52:27 +0100

    second

--- front/pfe/src/app/app.module.ts
+++ front/pfe/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
